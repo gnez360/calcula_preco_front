ff1942c7f89d528b3a1ef93b80d34c8e7a55d1a4
Guilherme Nery da Cruz <guilherme.cruz@sistemadivinaprovidencia.org>
2020-09-22 15:22:58 -0300

    update deploy

---
+++ dist/precache-manifest.4b3e7fb52a20f6d12f4f701d64e95329.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f3500504dc3f6b4733732318a05fc4e",
+    "url": "/calculapreco/Manifest.json"
+  },
+  {
+    "revision": "1250727a058199188806",
+    "url": "/calculapreco/css/app.611ee4c9.css"
+  },
+  {
+    "revision": "1a1fc263897652a96e3d1c7ce209cefd",
+    "url": "/calculapreco/img/logo.1a1fc263.png"
+  },
+  {
+    "revision": "aeac3340de2b25083bca62e3e8dce4b0",
+    "url": "/calculapreco/index.html"
+  },
+  {
+    "revision": "1250727a058199188806",
+    "url": "/calculapreco/js/app.ba67c64a.js"
+  },
+  {
+    "revision": "8d80c2e84a108909c240",
+    "url": "/calculapreco/js/chunk-vendors.ec149d99.js"
+  },
+  {
+    "revision": "40cb2d9509fa219d866c4cd72a8710da",
+    "url": "/calculapreco/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/calculapreco/robots.txt"
+  }
+]);
